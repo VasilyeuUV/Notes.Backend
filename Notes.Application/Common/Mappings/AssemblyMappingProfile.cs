4e79903fd0f488dbbd27beefff185604c867bd39
VasilyeuUV <vasilyeu.uv@mail.ru>
2023-06-20 00:52:10 +0300

    appl: mapping

---
+++ Notes.Application/Common/Mappings/AssemblyMappingProfile.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Notes.Application.Common.Mappings
+{
+    internal class AssemblyMappingProfile : Profile
+    {
+        /// <summary>
+        /// CTOR
+        /// </summary>
+        /// <param name="assembly"></param>
+        public AssemblyMappingProfile(Assembly assembly)
+            => ApplyMappingsFromAssembly(assembly);
+
+
+        private void ApplyMappingsFromAssembly(Assembly assembly)
+        {
+            var types = assembly.GetExportedTypes()                             // - сканирует сборку
+                .Where(type => type.GetInterfaces()                             // - ищет любые типы
+                    .Any(i => i.IsGenericType                                   // - которые реализуют
+                        && i.GetGenericTypeDefinition() == typeof(IMapWith<>))) // - интерфейс IMapWith
+                .ToList();
+
+            foreach (var type in types)
+            {
+                var instance = Activator.CreateInstance(type);
+                var methodInfo = type.GetMethod("Mapping");
+                methodInfo?.Invoke(instance, new object[] { this });
+            }
+        }
+    }
+}
---
+++ Notes.Application/Common/Mappings/IMapWith.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+
+namespace Notes.Application.Common.Mappings
+{
+    /// <summary>
+    /// Контракт для маппинга
+    /// </summary>
+    public interface IMapWith<T>
+    {
+        /// <summary>
+        /// Маппинг
+        /// </summary>
+        /// <param name="profile"></param>
+        void Mapping(Profile profile)
+            => profile.CreateMap(typeof(T), GetType());
+    }
+}
