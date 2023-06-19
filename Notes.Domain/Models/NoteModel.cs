b9266e69d0429e5c1e2ea27a4b3ed828625f4b3e
VasilyeuUV <vasilyeu.uv@mail.ru>
2023-06-19 22:30:15 +0300

    Добавьте файлы проекта.

---
+++ Notes.Domain/Models/NoteModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Notes.Domain.Models
+{
+    /// <summary>
+    /// Модель заметки
+    /// </summary>
+    public class NoteModel
+    {
+
+        /// <summary>
+        /// Id пользователя, добавившего заметку
+        /// </summary>
+        public Guid UserId { get; set; }
+
+        /// <summary>
+        /// Id заметки
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Название заметки
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Детали заметки
+        /// </summary>
+        public string Details { get; set; }
+
+        /// <summary>
+        /// Дата создания заметки
+        /// </summary>
+        public DateTime CreationDate { get; set; }
+
+        /// <summary>
+        /// Дата реадктирования заметки
+        /// </summary>
+        public DateTime? EditDate { get; set; }
+    }
+}
