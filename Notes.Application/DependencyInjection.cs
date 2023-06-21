89dc4172e8cb559068b5272881d780925e23e892
VasilyeuUV <vasilyeu.uv@mail.ru>
2023-06-21 13:49:20 +0300

    sln: add dependency injection

---
+++ Notes.Application/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Notes.Application
+{
+    /// <summary>
+    /// Расширение для регистрации MediatR
+    /// </summary>
+    public static class DependencyInjection
+    {
+
+        public static IServiceCollection AddAplication(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            return services;
+        }
+    }
+}
---
+++ Notes.Persistence/DependencyInjection.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Notes.Application.Interfaces;
+
+namespace Notes.Persistence
+{
+    /// <summary>
+    /// Расширение для добавления коннекта к БД
+    /// </summary>
+    public static class DependencyInjection
+    {
+        /// <summary>
+        /// Метод расширения для добавления использования контекста БД и регистрации его
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="configuration"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration["DbConnection"];
+            services.AddDbContext<NotesDbContext>(options =>
+            {
+                options.UseSqlite(connectionString);
+            });
+
+            services.AddScoped<INotesDbContext>(provider => provider.GetService<NotesDbContext>());
+            return services;
+        }
+    }
+}
